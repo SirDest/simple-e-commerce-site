a33d728fafa8d555cfc2da663f93d4ddae028265
Akinro Destined <104671422+SirDest@users.noreply.github.com>
2024-10-08 10:32:10 +0100

    commit

--- src/components/Loading.tsx
+++ src/components/Loading.tsx
@@ -1,7 +1,7 @@
 import React from "react";
 import { ClipLoader } from "react-spinners";
 
-const Loading: React.FC = () => {
+const Loading = () => {
   return (
     <div className='flex flex-col gap-2 text-[15px] md:text-[20px] text-center justify-center py-3'>
       <div className='m-auto flex items-center w-fit'>
--- src/components/Products/Electronics/ElectronicCard.tsx
+++ src/components/Products/Electronics/ElectronicCard.tsx
@@ -5,7 +5,7 @@ import Loading from "../../Loading";
 import ErrorPage from "../../ErrorPage";
 import Card from "../Card";
 
-const ElectronicCard: React.FC = () => {
+const ElectronicCard = () => {
   const [products, setProducts] = useState<Product[]>([]);
   const [loading, setLoading] = useState<boolean>(true);
   const [error, setError] = useState<string | null>(null);
